8159377aa04f087119934f0331cecb85ee52d3fd
Arafin Ahmed <arafin2021@gmail.com>
2021-03-31 10:30:36 +0600

    conntected to mongodb

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 8888;
+require('dotenv').config();
+app.use(bodyParser.json())
+app.use(cors());
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2jkon.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+
+
+
+client.connect(err => {
+  const productsCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COLLECTION);
+  const ordersCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COLLECTION2);
+  console.log("Connected");
+
+
+});
+
+
+
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+//database: 
+//collection: 
